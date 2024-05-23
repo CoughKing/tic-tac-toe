6b1d0c8e2e9f979f5e8727307a43514d4d927be4
unknown <cocfanlike@gmail.com>
2024-05-23 11:59:18 +0545

    nd

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,26 @@ function Square({value, onSquareClick}) {
 
 function App() {
 
+  const [xIsNext, setXIsNext] = useState(true);
+
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i){
+
+    if (squares[i]){
+      return;
+    }
     const nextSquares= squares.slice();
+    if (xIsNext)
+    {
     nextSquares[i] = "X";
+    }
+    else
+    {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
   
 
@@ -43,6 +57,29 @@ function App() {
       </div>
     </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+
+  ];
+  for (let i=0; i<lines.length; i++){
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a === squares[c]]){
+      return squares[a];
+    }
+  }
+
+  return null;
+
 }
 
 export default App
